5e4da5019d8cf52178e7c9795f2ebb046b58614e
Yako <jakooste@outlook.com>
2025-05-12 16:52:38 +0200

    add agent name in requests + refactor

--- Zaj10/czy_grali_razem.py
+++ Zaj10/czy_grali_razem.py
@@ -6,7 +6,9 @@ from urllib.error import URLError, HTTPError
 MAX_PAGE_LENGTH = 1000000
 MAX_PAGES = 1000
 
-def translate_response(response):
+AGENT_NAME = "Yako's Discogs API client +https://github.com/jakooste/PS"
+
+def translate_response_to_json(response):
     if response.status != 200:
         print("!!! Status not OK")
         sys.exit(1)
@@ -21,36 +23,39 @@ def translate_response(response):
     return response_json
 
 if __name__ == "__main__":
+
     if len(sys.argv) < 3:
-        print("Sposób użycia: czy_grali_razem.py [ARTYSTA1] [ARTYSTA2]")
+        print("Sposób użycia: czy_grali_razem.py [ARTYSTA1_ID] [ARTYSTA2_ID]")
         sys.exit(1)
+    
     artist1 = sys.argv[1]
     artist2 = sys.argv[2]
     result = []
     url = f"https://api.discogs.com/artists/{artist1}"
 
-    print(f"Sprawdzam, czy {artist1} i {artist2} grali razem w zespole.")
+    request = urllib.request.Request(url, headers={'User-Agent': AGENT_NAME})
+    with urllib.request.urlopen(request) as response:
+        
+        response_json = translate_response_to_json(response)
 
-    with urllib.request.urlopen(url) as response:
-        response_json = translate_response(response)
         if 'groups' not in response_json or len(response_json['groups']) == 0:
             print(f"Artysta 1 ({artist1}) nie grał w żadnym zespole.")
             sys.exit(0)
+        
         groups = response_json['groups']
-        # print(f"Artysta 1 ({artist1}) grał w {len(groups)} zespołach:")
+
         for group in groups:
-            # print(group['name'])
-            response2 = urllib.request.urlopen(group['resource_url'])
-            response2_json = translate_response(response2)
-            # print(f"Zespół {group['name']} ma {len(response2_json['members'])} członków:")
-            for member in response2_json['members']:
-                # print(f"{member['name']} ({member['id']})")
-                if str(member['id']) == artist2:
-                    # print(f"Artysta 2 ({artist2}) grał w zespole {group['name']}.")
-                    result.append(group['name'])
+            request2 = urllib.request.Request(group['resource_url'], headers={'User-Agent': AGENT_NAME})
+            with urllib.request.urlopen(request2) as response2:
+                response2_json = translate_response_to_json(response2)
+                for member in response2_json['members']:
+                    if str(member['id']) == artist2:
+                        result.append(group['name'])
     
     if len(result) == 0:
         print(f"Nie grali razem")
     else:
+        print(f"Grali razem w:")
+        result.sort()
         for group in result:
             print(group)
